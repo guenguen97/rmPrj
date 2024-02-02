7248d4c70a8d89b3456a8789e1390292a0f2d750
유ìguenguen97 <guenguen9797@gmail.com>
2024-02-03 02:18:56 +0900

    산출물 만드는중

--- src/main/java/com/rm/pay/KakaoPayController.java
+++ src/main/java/com/rm/pay/KakaoPayController.java
@@ -71,21 +71,23 @@ public class KakaoPayController {
 
 
         //구독 기록 저장하기, 구독 연장 신청시에는 따로 저장안하고 업데이트만 해주기
-        if(subscribeRequest.getRank()!=null) {
+        if(subscribeRequest.getRank() !=null) {
             subscribeService.makeSubscribe(subscribeRequest);
-        }else {
+        } else {
             System.out.println("구독 연장 신청 함수 !!!!!!!!!!!!!!!!!!!");
             SubscribeResponse originSub=subscribeService.getSubscribeByUserId(user.getId());
             //기존에 있던 구독 기간이랑 요청 들어온 구독 연장 기간 더하기
             subscribeRequest.setId(user.getId());
             subscribeRequest.setPeriod(subscribeRequest.getPeriod()+originSub.getPeriod());
+
+            System.out.println("현재 구독 id "+subscribeRequest.getId() +"현재 연장된 기간" +subscribeRequest.getPeriod());
              subscribeService.updatePeriod(subscribeRequest);
 
              //알림창 나오게 하기 위해 정보 전달
             redirectAttributes.addAttribute("alertKind", "updatePeriod");
 
             return "redirect:/";
-        }
+          }
 
         //알림창 나오게 하기 위해 정보 전달
         redirectAttributes.addAttribute("alertKind", "createSubscribe");
